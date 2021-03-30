c7dcfcd2c014dbccf03c0f3e17fa324b8092a6e7
Mark McDonald <mark.mcdonald@greenhouse.io>
2021-03-29 23:36:27 -0400

    Initial commit

---
+++ src/lib/puzzle.js
@@ -0,0 +1,28 @@
+const nextValue = (currentValue) => {
+	switch (currentValue) {
+		case "-":
+			return "|";
+		case "|":
+			return null;
+		default:
+			return "-";
+	}
+};
+
+export default class Puzzle {
+	constructor(puzzleData) {
+		this.puzzleData = puzzleData;
+	}
+
+	toggleOpening(rowIndex, cellIndex) {
+		debugger;
+		const newPuzzle = [...this.puzzleData];
+		const currentValue = newPuzzle[rowIndex][cellIndex];
+		newPuzzle[rowIndex][cellIndex] = nextValue(currentValue);
+		return new Puzzle(newPuzzle);
+	}
+
+	rows() {
+		return this.puzzleData;
+	}
+}
